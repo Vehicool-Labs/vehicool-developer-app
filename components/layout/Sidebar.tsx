e4f1fadc52bbfdbf8fed1b1df3a50406e6c92a9f
Bryan BERGER <contact@weberger.net>
2023-05-08 20:12:52 +0200

    fix(sidebar): fix heading

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -6,6 +6,7 @@ type SidebarProperties = {
 		href?: string;
 		target?: HTMLAttributeAnchorTarget;
 		className?: string;
+		tag?: string;
 	};
 	children: ReactNode;
 	className?: string;
@@ -16,10 +17,20 @@ const Sidebar: FC<SidebarProperties> = ({ brand, children, className = '' }) => 
 	return (
 		<div className={ `w-[200px] bg-blue-100 p-6 ${ className } flex flex-col items-center` }>
 			<a
-				className={ `text-2xl font-bold text-blue-500 mb-6 ${ brand.className }` }
+				className={ `flex flex-col mb-6 gap-0 ${ brand.className || '' }` }
 				href={ brand.href }
 				target={ brand.target || '_self' }
-			>{ brand.title }
+			>
+				<span className="text-3xl leading-7 font-bold text-blue-500">
+					{ brand.title }
+				</span>
+				{
+					brand.tag ?
+						<span className="text-sm font-medium pl-1 text-blue-400">
+							{ brand.tag }
+						</span>
+				 	: null
+				}
 			</a>
 			<div className="self-start w-full flex-1 flex flex-col">
 				{ children }
